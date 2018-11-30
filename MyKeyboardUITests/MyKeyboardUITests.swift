b14cf9560599f02bcd2a982a91536fa7f6fa1790
ebrimatunkara <ebrimatunkara@gmail.com>
2018-11-29 20:32:46 -0500

    Initial Commit

---
+++ MyKeyboardUITests/MyKeyboardUITests.swift
@@ -0,0 +1,34 @@
+//
+//  MyKeyboardUITests.swift
+//  MyKeyboardUITests
+//
+//  Created by Ebrima Tunkara on 11/29/18.
+//  Copyright © 2018 Ebrima Tunkara. All rights reserved.
+//
+
+import XCTest
+
+class MyKeyboardUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
